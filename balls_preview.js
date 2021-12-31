0e15eba410f0854dc7ef33c08167f58b17087713
杉山慶 <sugiyamakei@sugiyamkeinoMBP.fios-router.home>
2021-12-30 16:46:35 -0800

    added preview window for balls page

---
+++ balls_preview.js
@@ -0,0 +1,81 @@
+// The area object defines the div in which the balls will be created
+const area = {
+  element: document.getElementById('walk'),
+  height: 271,
+  width: 408,
+};
+area.element.style.width = area.width + 'px';
+area.element.style.height = area.height + 'px';
+
+// The moveTo function moves a given ball to a set x and y coordinates on the page
+function moveTo(ball, x, y) {
+  ball.element.style.left = x + 'px';
+  ball.element.style.top = y + 'px';
+}
+
+// The changeDirectionIfNecessary function reverses the ball direction when it hits the area borders
+function changeDirectionIfNecessary(ball, x, y) {
+  if (x < 0 || x >= area.width - ball.width) {
+    ball.dx = -ball.dx;
+  }
+  if (y < 0 || y >= area.height - ball.height) {
+    ball.dy = -ball.dy;
+  }
+}
+
+// TODO: implement the create function
+function create(color, dx, dy) {
+  const newBall = Object.create(this);
+
+  // TODO: Set newBall properties: dx, dy, width, height
+  newBall.dx = dx;
+  newBall.dy = dy;
+  newBall.width = 40;
+  newBall.height = 40;
+
+  // TODO: set the newBall.element property and initialize it to a Html element "div"
+  newBall.element = document.createElement('div');
+  // TODO: set the backgroundColor, width and height style properties for newBall.element
+  newBall.element.style.backgroundColor = color;
+  newBall.element.style.width = newBall.width + 'px';
+  newBall.element.style.height = newBall.height + 'px';
+  newBall.element.style.position = "absolute";
+  newBall.element.style.left = 0;
+  // This line set the CSS class for newBall.element. No need to change this line
+  newBall.element.className += 'ball';
+
+  // TODO: set the width and height of newBall based on newBall.element
+  // Hint: use the Javascript parseInt() function to convert a string value to integer
+  newBall.width = parseInt(newBall.element.style.width);
+  newBall.height = parseInt(newBall.element.style.height);
+  // TODO: use the Javascript appendChild() function to add newBall.element to the area element
+  area.element.appendChild(newBall.element);
+  return newBall;
+}
+
+// TODO: implement the update function
+function update(ball, x, y) {
+  // TODO: use the moveTo() function to move the ball
+  moveTo(ball, x, y);
+  // TODO: use the Javascript setTimeout() method to call changeDirectionIfNecessary() and update() every 16ms
+  setTimeout(function () {
+    changeDirectionIfNecessary(ball, x, y);
+    update(ball, x + ball.dx, y + ball.dy);
+  }, 1000 / 60);
+}
+
+// This is expected to create 3 balls within the area div
+
+const ball1 = create('blue', 2, 3);
+const ball2 = create('red', 4, 1);
+const ball3 = create('green', 2, 1);
+moveTo(ball1, 1, 1);
+moveTo(ball2, 10, 10);
+moveTo(ball3, 20, 20);
+
+// This is expected to make the 3 balls move around the area div
+
+ update(ball1, 70, 0);
+ update(ball2, 20, 20);
+ update(ball3, 50, 70);
+
